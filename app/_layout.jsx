2208c69d9ada7c7e102af2e159fdca3247ad85e4
MikiZenebe <mikizenebe10gmail.com>
2024-12-26 23:50:16 +0300

    tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,6 @@
 import { Slot, Stack } from "expo-router";
 import { View, Text } from "react-native";
+import "../global.css";
 
 export default function RootLayout() {
   return (
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,15 @@
 import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import "../global.css";
 
 export default function App() {
   return (
     <View>
-      <Text style={{ fontSize: "25px", textAlign: "center" }}>This is App</Text>
+      <Text style={{ textAlign: "center" }}>This is App</Text>
       <StatusBar style="auto" />
-      <Link style={{ color: "blue" }} href="/profile">
-        Go to profile
+      <Link style={{ color: "blue" }} href="/home">
+        Go to Home
       </Link>
     </View>
   );
