100c032303b12b6588595625c4ea73edd13af9fe
MikiZenebe <mikizenebe10gmail.com>
2024-12-30 21:39:05 +0300

    auth-screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -2,17 +2,24 @@ import { View, Text, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 import { useState } from "react";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[90vh] px-4 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -36,6 +43,25 @@ const SignIn = () => {
             handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles="mt-7"
           />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="flex-row justify-center gap-2 pt-5">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+              <Link
+                href="/sign-up"
+                className="text-lg font-psemibold text-secondary"
+              >
+                Sign Up
+              </Link>
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,15 @@
 import { Slot, Stack } from "expo-router";
 import { View, Text } from "react-native";
 import "../global.css";
+import { useEffect } from "react";
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="profile" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="/search/[query]" options={{ headerShown: false }} />
     </Stack>
   );
 }
